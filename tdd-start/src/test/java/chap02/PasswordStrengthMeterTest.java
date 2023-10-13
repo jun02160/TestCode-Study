3c72c08577d2659a8deb0da7cfd3bf868cabfb7b
jun02160 <jun020216@naver.com>
2023-10-13 14:34:08 +0900

    [2장] 실습-두 번째 테스트

--- tdd-start/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ tdd-start/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -16,4 +16,12 @@ public class PasswordStrengthMeterTest {
         PasswordStrength result2 = meter.meter("ab12!Add");
         assertEquals(PasswordStrength.STRONG, result2);
     }
+
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("Ab12!c");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
 }
