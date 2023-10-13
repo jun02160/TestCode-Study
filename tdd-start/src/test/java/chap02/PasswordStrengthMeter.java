7f0afb6cc3fada33c3ff47bfd98d880a59c4ff25
jun02160 <jun020216@naver.com>
2023-10-13 15:51:19 +0900

    [2장] 실습-아홉 번째 테스트 및 코드 정리

--- tdd-start/src/test/java/chap02/PasswordStrengthMeter.java
+++ tdd-start/src/test/java/chap02/PasswordStrengthMeter.java
@@ -5,17 +5,22 @@ public class PasswordStrengthMeter {
 
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
 
-        int metCounts = 0;
-
         // 개별 규칙을 검사하는 로직
-        if (s.length() >= 8) metCounts++;
-        if (meetsContainingNumberCriteria(s)) metCounts++;
-        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+        int metCounts = getMetCriteriaCounts(s);
 
         // 규칙을 검사한 결과에 따라 암호 강도를 계산하는 로직
-        if (metCounts == 1) return PasswordStrength.WEAK;
+        if (metCounts <= 1) return PasswordStrength.WEAK;
         if (metCounts == 2) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
+    }
+
+    private static boolean meetsContainingNumberCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean meetsContainingUppercaseCriteria(String s) {
@@ -27,12 +32,11 @@ public class PasswordStrengthMeter {
         return false;
     }
 
-    private static boolean meetsContainingNumberCriteria(String s) {
-        for (char ch : s.toCharArray()) {
-            if (ch >= '0' && ch <= '9') {
-                return true;
-            }
-        }
-        return false;
+    private static int getMetCriteriaCounts(String s) {
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+        return metCounts;
     }
 }
