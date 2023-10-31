8074bd8562bd6aabf86d5a496e26e5fee2c98562
jun02160 <jun020216@naver.com>
2023-10-31 18:38:57 +0900

    [3장] 첫 납부일을 추가한 PayData 클래스 생성

--- tdd-start/src/test/java/chap03/ExpiryDateCalculator.java
+++ tdd-start/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,7 +4,12 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+    public LocalDate calculateExpiryDate(PayData payData) {
+        if (payData.getFirstBillingDate() != null) {
+            if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
+                return LocalDate.of(2019, 3, 31);
+            }
+        }
+        return payData.getBillingDate().plusMonths(1);
     }
 }
