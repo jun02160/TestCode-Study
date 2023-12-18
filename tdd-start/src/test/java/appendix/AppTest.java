5e8a5e441089c82afc47b9b506a3c67f06962f8d
jun02160 <jun020216@naver.com>
2023-12-18 13:42:26 +0900

    [부록B] JUnit4 실습

---
+++ tdd-start/src/test/java/appendix/AppTest.java
@@ -0,0 +1,25 @@
+package appendix;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void plus() {
+        assertEquals(2, 1+1);
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
